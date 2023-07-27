398413613854a6389eb4dc91502f42659dda0cd1
arthurshk <arthurdragon39@gmail.com>
2023-07-27 16:16:58 +0300

    Add project files.

---
+++ Tomagachi/Actions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomagachi
+{
+    public class Actions
+    {
+        public event delegator eventz;
+        public void Toma(string task)
+        {
+            if (eventz != null)
+            {
+                eventz.Invoke(task);
+            }
+        }
+
+        public void Sleep(string task)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("I am Sleeping zzzzzzzzzzzzzzzzz!");
+            Console.ResetColor();
+        }
+        public void Eat(string task)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("I am eating YUM!");
+            Console.ResetColor();
+        }
+        public void Walk(string task)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("I am walking and exersizing in my neighborhood!");
+            Console.ResetColor();
+        }
+        public void Cure(string task)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("I am now healthy again YAY!");
+            Console.ResetColor();
+            
+        }
+        public void Play(string task)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("I am playing basketball with my owner!");
+            Console.ResetColor();
+            
+        }
+    }
+}
---
+++ Tomagachi/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace Tomagachi
+{
+    public delegate void delegator(string task);
+    public delegate void ElapsedEventHandler(Object source, System.Timers.ElapsedEventArgs e);
+    class Program
+    {
+        private static string task;
+        public static int count;
+
+        static void Main(string[] args)
+        {
+            Time(); 
+
+        }
+        public static void Time()
+        {
+            Actions actions = new Actions();
+            actions.eventz += actions.Play;
+            actions.eventz += actions.Sleep;
+            actions.eventz += actions.Eat;
+            actions.eventz += actions.Walk;
+            actions.eventz += actions.Cure;
+            var random = new Random();
+            List<delegator> delegators = new List<delegator>();
+            delegators.Add(actions.Play);
+            delegators.Add(actions.Sleep);
+            delegators.Add(actions.Eat);
+            delegators.Add(actions.Walk);
+            delegators.Add(actions.Cure);
+            delegators[random.Next(0, 4)](task);
+            
+            for (int i = 0; i < 120; i++)
+            {
+
+                System.Timers.Timer timer = new System.Timers.Timer(1000);
+                timer.Interval = 3000;
+                timer.Elapsed += Enabler;
+                timer.Start();
+                
+                if (i == 0)
+                {
+                    timer.Start();
+                    Console.WriteLine("The Timer has started");
+                }
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine(i);
+                if(i % 5 == 0)
+                {
+                    timer.Stop();
+                    DialogResult dialogResult = MessageBox.Show("Would you like to either Play, Sleep, Eat, Walk or Cure the Tomagachi?", "Alert!", MessageBoxButtons.YesNo);
+                    if(dialogResult == DialogResult.Yes)
+                    {
+                        timer.Start();
+                        delegators[random.Next(0, 4)](task);
+                    }
+                    else if(dialogResult == DialogResult.No)
+                    {
+                        count++;
+                        if (count == 3 || count == 6 || count == 9)
+                        {
+                            DialogResult dialogResult2 = MessageBox.Show("Your Tomagachi has fallen sick, would you like to cure it?", "Alert!", MessageBoxButtons.YesNo);
+                            if (dialogResult == DialogResult.Yes)
+                            {
+                                timer.Start();
+                                delegators[random.Next(0, 4)](task);
+                                
+                            }
+                            else
+                            {
+                                Console.WriteLine("The game is over! Tomagachi is dead!");
+                                timer.Stop();
+                                break;
+                            }
+                        }
+                    }
+                    
+                }
+                if(i == 120)
+                {
+                    timer.Stop();
+                    Console.WriteLine("The Timer has ended");
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+            Console.WriteLine("Game Over");
+        }
+        public static void Enabler(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Timer is going at {0}", e.SignalTime);
+        }
+    }
+}
+  
